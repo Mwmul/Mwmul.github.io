def4fbcc122645e734d73a77a0ea49761e5412cc
Mwmul <mwmul14@gmail.com>
2017-05-25 17:07:52 +0100

    added page

---
+++ webeboostin/index.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>WeBeBoostin Challenge modes!</title>
+	<meta name="description" content="WeBeBoostin Challenge mode gold service!" />
+	<meta name="keywords" content="WoW, Challenge, Mode, Boost, Carry, Help, Service, Warcraft, World, Of, Warcraft" />
+	<meta name="author" content="Martin Wells Mulholland" />
+	<link rel="shortcut icon" href="http://i.gyazo.com/61fc11016c198e86b5705d23b94feda4.png">
+	<link rel="stylesheet" type="text/css" href="normalize.css" />
+	<link rel="stylesheet" type="text/css" href="font-awesome.min.css" />
+	<link rel="stylesheet" type="text/css" href="demo.css" />
+	<link rel="stylesheet" type="text/css" href="card.css" />
+	<link rel="stylesheet" type="text/css" href="pattern.css" />
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	<!--[if IE]>
+	  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<script>
+	if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+		var root = document.getElementsByTagName('html')[0];
+		root.setAttribute('class', 'ff');
+	};
+	</script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-67849613-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<script type="text/javascript">
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-7243260-2']);
+_gaq.push(['_trackPageview']);
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+</script>
+</head>
+
+<body class="demo-3">
+	<div class="container">
+		<header class="codrops-header">
+		<div class="logo"> 
+			<div id="banner">
+				<img alt="" height="243" src="img/R1Logo2.png" width="557"></div>
+			<a href="TheTeam.html" style="margin-right: 15px">The Team</a>
+			<a href="http://Twitch.tv" style="margin-right: 15px">Media</a>
+			<a href="FAQ.html" style="margin-right: 15px">FAQ</a>
+			<a href="TermsOfUse.html">Terms of Use</a></div>
+			<h1><span>Cheap, Fast, Reliable boosting team!</span></h1>
+		</header>
+		<div class="content">
+					<div class="pattern pattern--hidden"></div>
+			
+			<div class="wrapper" style="margin-top: -40px">
+				<div class="card">
+					<div class="card__container card__container--closed">
+						<svg class="card__image" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1920 500" preserveAspectRatio="xMidYMid slice">
+							<defs>
+								<clipPath id="clipPath1">
+									<polygon class="clip" points="0,500 0,0 1920,0 1920,500"></polygon>
+								</clipPath>
+							</defs>
+							<image clip-path="url(#clipPath1)" width="1920" height="500" xlink:href="img/a.jpg"></image>
+						</svg>
+						<div class="card__content">
+							<i class="card__btn-close fa fa-times">Back</i>
+							<div class="card__caption">
+								<h2 class="card__title">8/8 Gold Challenge Mode 
+								SelfPlay</h2>
+								<p class="card__subtitle">Price: 55 EURO </p>
+							</div>
+							<div class="card__copy">
+								<div class="meta">
+									<img src="1.png" alt="author01" width="800"/>
+									<span class="meta__author"></span>
+									<span class="meta__date"></span>
+								</div>
+								<p>This service will take you through a full run of gold challenge modes with us while you play your own character. You will be rewarded with 27 different achievements, a ground mount, a title, and 18 unique transmog weapons.</p>
+								<p><b>How long will it take?</b><br />The run will take between 1hour 30 minutes and 2 hours so you are required to have arround 2hours of time to complete this. (some breaks are allowed throughout the run if needed)</p>
+								<p><b>How is it done?</b><br />After you place your order we will contact you as soon as possible (usualy within an hour) over skype, There we will handle the payment (this can be left til minutes beffore the scheduled time if you wish.) and organise a time for your run that suits you. During the run you are not required to do any dps but we will guide you through the dungeon as we go to make it as easy as possible.</p>
+								<p></p>
+								<p></p>
+								<div id="form1">
+							<form action="send1.php" method="post">
+							<label>Order Now.</label>
+		
+						
+							<input name="Text1" type="text" placeholder="Name"/>
+						
+							<input name="Text2" type="text" required  placeholder="Email"/>
+							
+							<input name="Text3" type="text" placeholder="Armory - you may use www.maskedarmory.com for security if you wish."/>
+							
+							
+							<input name="Text4" type="text" placeholder="Skype name"/>
+							
+							
+							<input name="Text5" type="text"  placeholder="Referal Code">
+							
+							<label>I <b>Agree</b> to the WeBeBoostin Terms of Use: </label>
+							<br />
+							<label>Yes</label>
+							<input class="check" name="checkbox1" type="checkbox" required/>
+							<label>No</label>
+							 <input class="check" name="checkbox2" type="checkbox"/>
+							 <br/>
+							<br/>
+							<input name="Submit1" type="submit" value="Place Order" />
+							(Payment will be dealt with later)
+						
+							</form>
+						</div>
+
+								
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="card">
+					<div class="card__container card__container--closed">
+						<svg class="card__image" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1920 500" preserveAspectRatio="xMidYMid slice">
+							<defs>
+								<clipPath id="clipPath2">
+									<polygon class="clip" points="0,500 0,0 1920,0 1920,500"></polygon>
+								</clipPath>
+							</defs>
+							<image clip-path="url(#clipPath2)" width="1920" height="500" xlink:href="img/b.jpg"></image>
+						</svg>
+						<div class="card__content">
+							<i class="card__btn-close fa fa-times">Back</i>
+							<div class="card__caption">
+								<h2 class="card__title">8/8 Gold Challenge Mode  
+								Account Share</h2>
+								<p class="card__subtitle">Price: 50 EURO or 200k 
+								Gold on any server</p>
+							</div>
+							<div class="card__copy">
+								<div class="meta">
+									<img src="1.png" alt="author02" width="800px"/>
+									<span class="meta__author"></span>
+									<span class="meta__date"></span>
+								</div>
+								<p>This service requires us to play your character through a full run of gold challenge modes. Once completed your character will have 27 different achievements, a mount, a title and 18 unique transmog weapons waiting at the vendor for you to buy.</p>
+								<p><b>How long will it take?</b><br />The run will take 2hours 30minutes at MOST (you will be unable to use your account during this time)</p>
+								<p><b>How is it done?</b><br />After you place your order we will contact you as soon as possible (usualy within an hour) over skype, There we will handle the payment (this can be left til minutes beffore the scheduled time if you wish.) and organise a time for your run that suits you. We will ask for access to your account arround 1hour-30minutes beffore the scheduled time in order for us to settup UI/Keybinds etc..</p>
+								<p></p>
+								<p></p>
+								<div id="form2" >
+								<form action="send1.php" method="post">
+							<label>Order Now.</label>
+		
+						
+							<input name="Text1" type="text" placeholder="Name"/>
+						
+							<input name="Text2" type="text" required  placeholder="Email"/>
+							
+							<input name="Text3" type="text" placeholder="Armory - you may use www.maskedarmory.com for security if you wish."/>
+							
+							
+							<input name="Text4" type="text" placeholder="Skype name"/>
+							
+							
+							<input name="Text5" type="text"  placeholder="Referal Code">
+							
+							<label>I <b>Agree</b> to the WeBeBoostin Terms of Use: /label>
+							<br />
+							<label>Yes</label>
+							<input class="check" name="checkbox1" type="checkbox" required/>
+							 <label>No</label>
+							 <input class="check" name="checkbox2" type="checkbox"/>
+							 <br/>
+							<br/>
+							<input name="Submit1" type="submit" value="Place Order" />
+							(Payment will be dealt with later)
+						
+							</form>
+						</div>
+
+								
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="card">
+					<div class="card__container card__container--closed">
+						<svg class="card__image" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1920 500" preserveAspectRatio="xMidYMid slice">
+							<defs>
+								<clipPath id="clipPath3">
+									<polygon class="clip" points="0,500 0,0 1920,0 1920,500"></polygon>
+								</clipPath>
+							</defs>
+							<image clip-path="url(#clipPath3)" width="1920" height="500" xlink:href="img/c.jpg"></image>
+						</svg>
+						<div class="card__content">
+							<i class="card__btn-close fa fa-times">Back</i>
+							<div class="card__caption">
+								<h2 class="card__title">Customer Feedback</h2>
+								<p class="card__subtitle"></p>
+							</div>
+							<div class="card__copy">
+								<div class="meta">
+									<div class="feedback">
+										<img src="https://i.gyazo.com/a35cb98b67839466e0320f20758fd3d7.png" alt="">
+									</div>
+									
+								</div>
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+			<div class="about">
+			
+				<h3>About Us</h3>
+				<p>
+				WeBeBoostin is a group of Gamers from across the <b>world</b> who have a passion for World of Warcraft and specificaly Challenge Mode dungeons!<br />
+				We are here to provide you with the best possible service to 
+				complete your 8/8 gold challenge modes in <b>under two hours.</b><br />
+				Our team consists of 4 skilled players who have been exclusively playing challenge modes for over 2 years now.<br />
+				We also have several skilled backup players ready to step in at any time to ensure your run is completed with no hassle and remains fast and most of all fun!
+				</p>
+		</div>
+			<center>
+			<br>Contact us on Skype<br>
+			
+			<a rel="notfollow" href="skype:abysalm0nkey?add" target="blank">
+			<img alt="" src="http://www.drclaudiaonline.com/images/skype_logo.png" height="63" width="117"/>
+</a>
+
+			
+			<br>
+			<a href="" target="_blank">
+			<em>
+			<span class="auto-style3">Site Views</span></em><br />
+<img src="http://hitwebcounter.com/counter/counter.php?page=6095719&style=0008&nbdigits=5&type=page&initCount=0" title="Hit Counter" Alt="Hit Counter"   border="0" >
+</a>
+<br/>
+<!-- hitwebcounter.com --><a href="" title="Page Views" 
+target="_blank" style="font-family: Arial, Helvetica, sans-serif; 
+font-size: 11px; color: #9A96A3; text-decoration: underline ;">
+</a> </center>
+		
+	</div>
+	<script src="trianglify.min.js"></script>
+	<script src="TweenMax.min.js"></script>
+	<script src="ScrollToPlugin.min.js"></script>
+	<script src="cash.min.js"></script>
+	<script src="Card-polygon.js"></script>
+	<script src="demo-3.js"></script>
+<script src="demoad.js"></script>
+
+</body>
+
+
+</html>
