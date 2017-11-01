e4bc0a098664efec7b61d642c9427675f57f5e84
Mwmul <mwmul14@gmail.com>
2017-11-01 23:06:52 +0000

    fixed header

--- projectOne/js/index.js
+++ projectOne/js/index.js
@@ -1,5 +1,5 @@
 function title() {
-  var sct = document.body.scrollTop;
+  var sct = document.documentElement.scrollTop
   var intro = document.getElementById('intro');
   intro.style.transform =  "translate(0%," + sct/1.5 + "px";
   var menu = document.querySelector('NAV');
@@ -12,20 +12,9 @@ function title() {
 
 document.addEventListener('scroll', title);
 
-var menuBtn = document.getElementById('menu-btn');
-var closeBtn = document.getElementById('close-btn')
-function menuDrop(){
-	var menu = document.querySelector('nav UL');
-	menu.style.height = "160px";
-	menuBtn.style.display = "none";
-	closeBtn.style.display = "block"
-}
-function menuClose(){
-	var menu = document.querySelector('nav UL');
-	menu.style.height = "0";
-	menuBtn.style.display = "block";
-	closeBtn.style.display = "none"
-}
-menuBtn.addEventListener('click', menuDrop);
-closeBtn.addEventListener('click', menuClose);
 
+$(document).ready(function() {
+  $('header > .intro > nav > ul > li > .fa-bars').parent().click(function() {
+    $('header > .intro > nav > ul').toggleClass('expand')
+  })
+})
