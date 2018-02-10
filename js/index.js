ccc977a1bb1cfc19aed1b3d51f54ca302fe4eb73
Mwmul <mwmul14@gmail.com>
2018-02-10 23:36:02 +0000

    updated jQuery on portfolio home page

--- js/index.js
+++ js/index.js
@@ -1,24 +1,16 @@
-$('#about').click(function()
-	{
-		$('.container').css("opacity", "0")
-		$('.about').css("display", "block")
-	})
+$('.close').click(function () {
+	$('.about').css("display", "none");
+	$('.portfolio').css("display", "none");
+	$('.contact').css("display", "none");
+	$('.container').css("opacity", "1");
 
-$('.close').click(function()
-	{
-		$('.about').css("display", "none")
-		$('.portfolio').css("display", "none")
-		$('.contact').css("display", "none")
-		$('.container').css("opacity", "1")
-	})
+});
 
-$('#portfolio').click(function()
-	{
+$(function() {
+  $('.option-container').click(function(){
 		$('.container').css("opacity", "0")
-		$('.portfolio').css("display", "block")
-	})
-$('#contact').click(function()
-	{
-		$('.container').css("opacity", "0")
-		$('.contact').css("display", "block")
-	})
+		console.log('test');
+    var panelid = $(this).attr('data-panelid');
+		$('#'+panelid).fadeToggle(500);
+  });
+});
